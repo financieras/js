2a5c634fb780d8fb4cc699cc48a1437caf33d351
Adolfo Aparicio <financieras@gmail.com>
2016-10-23 22:25:09 +0200

    Update mcdmcm.js

--- mcdmcm/mcdmcm.js
+++ mcdmcm/mcdmcm.js
@@ -1,17 +1,11 @@
 document.write('<h1>Máximo común divisor (MCD) y mínimo comúm múltiplo (mcm)</h1>');
 document.write('<h2>Dados dos números calcular el MCD y el mcm</h2>');
-do{  
+do{
 	var n1 = prompt("Dime el primer número (de 1 a un billón)", 9805035240);
 }while(isNaN(n1) || n1> 1000000000000 || n1< 1 || n1%1!==0);
-do{  
+do{
 	var n2 = prompt("Dime el segundo número (de 1 a un billón)", 65418210);
 }while(isNaN(n2) || n2> 1000000000000 || n2< 1 || n2%1!==0);
-
-var fila=0;
-var A = new Array(12);
-	for(var k = 1; k <= 3; k++) {
-		A[k] = new Array(3);
-	}
 
 for (var j=1;j<=2;j++){
 	if (j===1){
@@ -26,12 +20,6 @@ for (var j=1;j<=2;j++){
     	n=n/2;
     	contador++;
 	}
-	if (contador>0){
-		A[3][1]=2;
-		A[j][1]=contador;
-		if (A[1][1] === undefined) {A[1][1]=0}
-		if (A[2][1] === undefined) {A[2][1]=0}
-	}
 	if (contador>1){
 		texto += 2+"^"+contador+"x";
 	} else if (contador===1){
@@ -44,31 +32,6 @@ for (var j=1;j<=2;j++){
     		n=n/i;
     		contador++;
 		}
-		if (contador>0){
-			if (j===1){
-				fila++;
-				A[3][fila]=i;
-				A[1][fila]=contador;
-				if (A[1][fila] === undefined) {A[1][fila]=0}
-				if (A[2][fila] === undefined) {A[2][fila]=0}
-			} else if (j===2){
-				var repe=false;
-				for (p=1;p<=fila;p++){
-					if (i===A[3][p]){
-						A[2][p]=contador;
-						repe=true;
-						break;
-					}
-				}
-				if (!(repe)){
-					fila++;
-					A[3][fila]=i;
-					A[2][fila]=contador;
-					if (A[1][fila] === undefined) {A[1][fila]=0}
-					if (A[2][fila] === undefined) {A[2][fila]=0}
-				}
-			}
-		}
 		if (contador>1){
 			texto += i+"^"+contador+"x";
 		} else if (contador===1){
@@ -78,8 +41,4 @@ for (var j=1;j<=2;j++){
 	
 	texto += 1;
 	document.write(texto+'<br>');
-}
-
-for(s=1;s<=fila;s++){		
-	document.write ("fila "+s+"="+A[3][s]+", "+A[1][s]+", "+A[2][s]+'<br>');
 }
