e80146fce83de1d524aff7143f3f1cda8adf3e56
Adolfo Aparicio <financieras@gmail.com>
2016-10-23 22:57:26 +0200

    Update mcdmcm.js

--- mcdmcm/mcdmcm.js
+++ mcdmcm/mcdmcm.js
@@ -1,11 +1,17 @@
 document.write('<h1>Máximo común divisor (MCD) y mínimo comúm múltiplo (mcm)</h1>');
 document.write('<h2>Dados dos números calcular el MCD y el mcm</h2>');
 do{
-	var n1 = prompt("Dime el primer número (de 1 a un billón)", 9805035240);
+	var n1 = prompt("Dime el primer número (de 1 a un billón)", 180);
 }while(isNaN(n1) || n1> 1000000000000 || n1< 1 || n1%1!==0);
 do{
-	var n2 = prompt("Dime el segundo número (de 1 a un billón)", 65418210);
+	var n2 = prompt("Dime el segundo número (de 1 a un billón)", 1032);
 }while(isNaN(n2) || n2> 1000000000000 || n2< 1 || n2%1!==0);
+
+if (n2>n1){                                    //queremos que n1 sea el mayor
+	var z=n1;
+	n1=n2;
+	n2=z
+}
 
 for (var j=1;j<=2;j++){
 	if (j===1){
@@ -42,3 +48,12 @@ for (var j=1;j<=2;j++){
 	texto += 1;
 	document.write(texto+'<br>');
 }
+
+do {                                                    //algoritmo de Euclides
+	var mcd=n2;
+	var resto=n1%n2;
+	n1=n2;
+	n2=resto;
+} while (resto=0);
+
+document.write('El MCD es "+mcd+'<br>');
