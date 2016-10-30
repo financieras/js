9a2a40dc0fa6afabc1eb5cc3603ee2c4f792f626
Adolfo Aparicio <financieras@gmail.com>
2016-10-30 20:50:42 +0100

    Update mcdmcm2.js

--- mcdmcm2/mcdmcm2.js
+++ mcdmcm2/mcdmcm2.js
@@ -6,10 +6,11 @@ function myFunction() {
     z = document.getElementById("num3").value;
     if (isNaN(x) || isNaN(y)) {
         text = "Es necesarios introducir dos números válidos. El tercer valor es opcional.";
+        document.getElementById("calculaMCDmcm").style.color="#FF0000";
     } else {
         suma=parseFloat(x)+parseFloat(y)+parseFloat(z);
         text= suma;
     }
     document.getElementById("calculaMCDmcm").innerHTML = text;
-    //document.getElementById("calculaMCDmcm").style.color="#FF0000";
+    
 }
